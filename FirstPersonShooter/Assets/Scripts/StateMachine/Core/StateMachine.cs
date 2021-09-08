dc95e71cb3cb050d90436e0bdb602c4e05bdec96
RFS-6ro <56915388+RFS-6ro@users.noreply.github.com>
2021-09-08 23:47:50 +0300

    State Machine Initial commit

---
+++ FirstPersonShooter/Assets/Scripts/StateMachine/Core/StateMachine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StateMachine
+{
+    public abstract class StateMachine
+    {
+        protected State _currentState;
+
+        public void AddTransition(State from, State to)
+        {
+
+        }
+    }
+}
