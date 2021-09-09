9750ec1fe8e6a92efdce7c4d9c72125782ec4fef
RFS-6ro <56915388+RFS-6ro@users.noreply.github.com>
2021-09-09 11:53:14 +0300

    Adding Extensions Folder starts with float extensions

---
+++ FirstPersonShooter/Assets/Scripts/Extensions/Value Types/FloatExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Extensions
+{
+    public static class FloatExtensions
+    {
+        public static bool Compare(this float value, float other, float epsilon = 0.0001f)
+        {
+            float difference = UnityEngine.Mathf.Abs(value * epsilon);
+
+            return UnityEngine.Mathf.Abs(value - other) <= difference;
+        }
+    }
+}
