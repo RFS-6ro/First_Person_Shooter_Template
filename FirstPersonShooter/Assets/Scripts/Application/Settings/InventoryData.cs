5ce1ac9926252ef0de14af5449ed308c3267dd4b
RFS-6ro <56915388+RFS-6ro@users.noreply.github.com>
2021-09-09 11:46:35 +0300

    Updating Settings Datas and adding Inventory Data

---
+++ FirstPersonShooter/Assets/Scripts/Application/Settings/InventoryData.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Types;
+using UnityEngine;
+
+namespace Settings
+{
+    [CreateAssetMenu(fileName = "NewInventoryData", menuName = "Settings/Create Inventory Data")]
+    public class InventoryData : ScriptableObject
+    {
+        #region Granades
+        [System.Serializable]
+        public struct GranadeInventoryData
+        {
+            public GranadeType Type;
+            public int Limit;
+
+            public GranadeInventoryData(GranadeType type, int limit)
+            {
+                Type = type;
+                Limit = limit;
+            }
+        }
+
+        [Header("Granades")]
+        [SerializeField] private GranadeInventoryData[] _granades =
+        {
+            new GranadeInventoryData(GranadeType.Type1, 5),
+            new GranadeInventoryData(GranadeType.Type2, 5),
+            new GranadeInventoryData(GranadeType.Type3, 5),
+        };
+
+        public Dictionary<GranadeType, int> Granades;
+        #endregion
+
+
+
+        private void OnValidate()
+        {
+            #region Granades
+
+            if (Granades == null)
+            {
+                Granades = new Dictionary<GranadeType, int>();
+            }
+
+            for (int i = 0; i < _granades.Length; i++)
+            {
+                Granades.Add(_granades[i].Type, _granades[i].Limit);
+            }
+
+            _granades = Granades.Select((data) => new GranadeInventoryData(data.Key, data.Value)).ToArray();
+
+            #endregion
+        }
+    }
+}
--- FirstPersonShooter/Assets/Scripts/Application/Settings/PlayerMovementData.cs
+++ FirstPersonShooter/Assets/Scripts/Application/Settings/PlayerMovementData.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewInputData", menuName = "Settings/Create Input Data")]
-public class PlayerMovementData : ScriptableObject
+namespace Settings
 {
+    [CreateAssetMenu(fileName = "NewPlayerMovementData", menuName = "Settings/Create Player Movement Data")]
+    public class PlayerMovementData : ScriptableObject
+    {
+        [Header("Movement")]
+        public float MovementVelocity = 10f;
 
+        [Header("Interaction")]
+        public float ForceLimit = 10f;
+    }
 }
