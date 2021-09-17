f7cb4b3c80d745385f0bded5509e3f64c6a3186b
RFS-6ro <56915388+RFS-6ro@users.noreply.github.com>
2021-09-18 02:36:41 +0300

    Updating DIResolver with comment of DI template

---
+++ FirstPersonShooter/Assets/Scripts/Application/Pseudo DI/DIResolver.cs
@@ -0,0 +1,14 @@
+﻿namespace DI
+{
+    public class DIResolver
+    {
+        //var ignoreType = typeof(IgnoreInjectAttribute);
+        //foreach (var field in class.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+        //{
+        //  if (field.IsStatic || Attribute.IsDefined(field, ignoreType))
+        //  {
+        //      continue;
+        //  }
+        //}
+    }
+}
